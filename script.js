b26c7d3a298b35fff8316f5cf534d90b38c8ae6c
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-13 18:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
+let btn=document.querySelector("#addTodoBtn")
+let text=document.querySelector("#newTodoInput")
+let orderlist=document.querySelector("#todoList")
+btn.addEventListener("click", list)
+function list() {
+	let l=document.createElement("li")
+	l.innerText=text.value;
+	orderlist.appendChild(l)
+	text.value="";
+	
+}
 
