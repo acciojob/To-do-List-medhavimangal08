ae5c7667c9cc02ba91535ede51be2d2a36277ac2
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-13 18:08:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let text=document.querySelector("#newTodoInput")
 let orderlist=document.querySelector("#todoList")
 btn.addEventListener("click", list)
 function list() {
-	let l=document.createElement("li")
-	l.innerText=text.value;
-	orderlist.appendChild(l)
-	text.value="";
-	
+	if(text.value!=="") {
+		let l = document.createElement("li");
+		l.innerText = text.value;
+		orderlist.appendChild(l);
+		text.value = "";
+	}
 }
-
